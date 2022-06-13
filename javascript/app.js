230e0c6d2229a55016528c93c3e34e0e149d34ca
Davi Grah <davi_laurindo@hotmail.com>
2022-06-13 11:23:58 -0300

    Notify dropdown menu done

--- javascript/app.js
+++ javascript/app.js
@@ -1,175 +1,172 @@
-// ---> CONST ---> 
+// ---> CONST --->
 
 const alertBanner = document.getElementById("alert");
-const trafficCanvas =document.getElementById("traffic-chart");
+const trafficCanvas = document.getElementById("traffic-chart");
 const dailyCanvas = document.getElementById("daily-chart");
 const mobileCanvas = document.getElementById("mobile-chart");
 const user = document.getElementById("userField");
 const message = document.getElementById("messageField");
 const send = document.getElementById("send");
 const box = document.getElementById("box");
-const boxItem = document.getElementById("box-item");
+const boxItem = document.querySelectorAll(".notify-close");
 var down = false;
-const bell = document.getElementsByClassName("bell");
+const bell = document.getElementsByClassName("bell")[0];
 
+// ---> notification box --->
 
-
-// ---> notification box ---> 
-
-function toggleNotifi(){
-  if (down) {
-    box.style.height = '0px';
-    box.style.opacity = 0;
-    box.classList.remove('show')
-    down = false;
-  } else {
-    box.style.height = 'fit-content';
-    box.style.opacity = 1;
+document.querySelector(".bell").addEventListener("click", (e) => {
+  if (e.target === bell) {
     box.classList.toggle("show");
-    down = true;
   }
-}
+});
 
-// ---> ALERT BANNER ---> 
-alertBanner.innerHTML = 
-` <div class="alert-banner">
+// ---> ALERT BANNER --->
+alertBanner.innerHTML = ` <div class="alert-banner">
 <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
 to complete</p>
 <p class="alert-banner-close">x</p>
-</div>`
+</div>`;
 
-// ---> EVENT LISTENER TO CLOSE THE ALERT BANNER ---> 
+// ---> EVENT LISTENER TO CLOSE THE ALERT BANNER --->
 
-alertBanner.addEventListener('click', e => {
-    const element = e.target;
-    if (element.classList.contains("alert-banner-close")) {
-    alertBanner.style.display = "none"
-    }
+alertBanner.addEventListener("click", (e) => {
+  const element = e.target;
+  if (element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none";
+  }
 });
 
-// ---> EVENT LISTENER TO CLOSE THE NOTIFICATION BANNER ---> 
-boxItem.addEventListener('click', e => {
-  const element = e.target;
-  if (element.classList.contains("notifi-close")) {
-  boxItem.style.display = "none"
-  }
-  
-} );
-
-
+// ---> EVENT LISTENER TO CLOSE THE NOTIFICATION BANNER --->
+boxItem.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const button = e.target;
+    button.closest(".notify-item").remove();
+  });
+});
 
 let trafficData = {
-    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
-    "4-10", "11-17", "18-24", "25-31"],
-    datasets: [{
-    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
-    2500],
-    backgroundColor: 'rgba(116, 119, 191, .3)',
-    borderWidth: 1,
-    }]
+  labels: [
+    "16-22",
+    "23-29",
+    "30-5",
+    "6-12",
+    "13-19",
+    "20-26",
+    "27-3",
+    "4-10",
+    "11-17",
+    "18-24",
+    "25-31",
+  ],
+  datasets: [
+    {
+      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+      backgroundColor: "rgba(116, 119, 191, .3)",
+      borderWidth: 1,
+    },
+  ],
 };
 
 let trafficOptions = {
-    backgroundColor: 'rgba(112, 104, 201, .5)',
-    fill: true,
-    aspectRatio: 2.5,
-    animation: {
-      duration: 0
+  backgroundColor: "rgba(112, 104, 201, .5)",
+  fill: true,
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0,
+  },
+  scales: {
+    y: {
+      beginAtZero: true,
     },
-    scales: {
-      y: {
-       beginAtZero: true
-     }
+  },
+  plugins: {
+    legend: {
+      display: false,
     },
-    plugins: {
-      legend: {
-      display: false
-    }
-  }
+  },
 };
 
 let trafficChart = new Chart(trafficCanvas, {
-    type: 'line',
-    data: trafficData,
-    options: trafficOptions
-    });
+  type: "line",
+  data: trafficData,
+  options: trafficOptions,
+});
 
-    // data for daily traffic bar chart
+// data for daily traffic bar chart
 const dailyData = {
   labels: ["S", "M", "T", "W", "T", "F", "S"],
-  datasets: [{
-    label: '# of Hits',
-    data: [75, 115, 175, 125, 225, 200, 100],
-    backgroundColor: '#7477BF',
-    borderWidth: 1
-    }]
-  };
-
-  const dailyOptions = {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
+  datasets: [
+    {
+      label: "# of Hits",
+      data: [75, 115, 175, 125, 225, 200, 100],
+      backgroundColor: "#7477BF",
+      borderWidth: 1,
     },
+  ],
+};
+
+const dailyOptions = {
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
   plugins: {
     legend: {
-    display: false
-      }
-    }
-  };
+      display: false,
+    },
+  },
+};
 
-    // data for mobile users bar chart
-  let dailyCanvastwo = new Chart(dailyCanvas, {
-    type: 'bar',
-    data: dailyData,
-    options: dailyOptions
-    });
+// data for mobile users bar chart
+let dailyCanvastwo = new Chart(dailyCanvas, {
+  type: "bar",
+  data: dailyData,
+  options: dailyOptions,
+});
 
-    const mobileData = {
-      labels: ["Desktop", "Tablet", "Phones"],
-      datasets: [{
-        label: '# of Users',
-        data: [2000, 550, 500],
-        borderWidth: 0,
-        backgroundColor: [
-          '#7477BF',
-          '#78CF82',
-          '#51B6C8'
-         ]
-      }]
-    };
+const mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [
+    {
+      label: "# of Users",
+      data: [2000, 550, 500],
+      borderWidth: 0,
+      backgroundColor: ["#7477BF", "#78CF82", "#51B6C8"],
+    },
+  ],
+};
 
-    const mobileOptions = {
-      aspectRatio: 1.9,
-      plugins: {
-        legend: {
-          position: 'right',
-          labels: {
-            boxWidth: 20,
-            fontStyle: 'bold'
-            }
-          }
-        }
-      };
+const mobileOptions = {
+  aspectRatio: 1.9,
+  plugins: {
+    legend: {
+      position: "right",
+      labels: {
+        boxWidth: 20,
+        fontStyle: "bold",
+      },
+    },
+  },
+};
 
-      let mobileChart = new Chart(mobileCanvas, {
-        type: 'doughnut',
-        data: mobileData,
-        options: mobileOptions
-        });
+let mobileChart = new Chart(mobileCanvas, {
+  type: "doughnut",
+  data: mobileData,
+  options: mobileOptions,
+});
 
+// --->  messagig section --->>
 
-// --->  messagig section --->> 
-
-send.addEventListener('click', () => {
+send.addEventListener("click", () => {
   // ensure user and message fields are filled out
   if (user.value === "" && message.value === "") {
     alert("Please fill out user and message fields before sending");
-  } else if (user.value === "" ) {
+  } else if (user.value === "") {
     alert("Please fill out user field before sending");
-  } else if (message.value === "" ) {
+  } else if (message.value === "") {
     alert("Please fill out message field before sending");
   } else {
     alert(`Message successfully sent to: ${user.value}`);
   }
-  });
+});
